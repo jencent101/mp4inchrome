ab6f287e0e5e66b4f6da30e9e6cd317ddd02ec9b
I-am-Programmer-101 <92503713+I-am-Programmer-101@users.noreply.github.com>
2021-10-14 00:17:18 -0700

    Add files via upload

---
+++ mp4inchrome.py
@@ -0,0 +1,19 @@
+import webbrowser
+
+def movieinchrome():
+    chromedir= 'C:/Program Files/Microsoft/Edge/Application/msedge.exe %s'
+    mp4file = input("mp4 file: ")
+    webbrowser.get(chromedir).open(mp4file)
+
+def songinchrome():
+    chromedir= 'C:/Program Files/Microsoft/Edge/Application/msedge.exe %s'
+    mp3file = input("mp3 file: ")
+    webbrowser.get(chromedir).open(mp3file)
+
+prompt = int(input("What do you want to play mp3 or mp4 [3/4]: "))
+if prompt == 3:
+    songinchrome()
+elif prompt == 4:
+    movieinchrome()
+else:
+    print("Invalid input")
